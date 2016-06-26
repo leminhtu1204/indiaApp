9294a181538f84616c11eaf321e1492ac623c656
Trung Trinh <trinhtoantrung@gmail.com>
2016-06-26 18:12:34 +0700

    Validation email, password match

--- PropertyManagement/www/views/register/registerController.js
+++ PropertyManagement/www/views/register/registerController.js
@@ -1,5 +1,6 @@
 ﻿angular.module('Belowval.Register', ['Belowval.LoginService'])
 
+
     .controller('registerController', function ($scope, $state, UserLogin, $ionicPopup, $timeout, $ionicLoading) {
 
         $scope.register = function (data) {
@@ -53,4 +54,39 @@
         $scope.hide = function () {
             $ionicLoading.hide();
         };
-    });
+
+    })
+
+    .directive("passwordVerify", function () {
+        return {
+            require: "ngModel",
+            scope: {
+                passwordVerify: '='
+            },
+            link: function (scope, element, attrs, ctrl) {
+                scope.$watch(function () {
+                    var combined;
+
+                    if (scope.passwordVerify || ctrl.$viewValue) {
+                        combined = scope.passwordVerify + '_' + ctrl.$viewValue;
+                    }
+                    return combined;
+                }, function (value) {
+                    if (value) {
+                        ctrl.$parsers.unshift(function (viewValue) {
+                            var origin = scope.passwordVerify;
+                            if (origin !== viewValue) {
+                                ctrl.$setValidity("passwordVerify", false);
+                                return undefined;
+                            } else {
+                                ctrl.$setValidity("passwordVerify", true);
+                                return viewValue;
+                            }
+                        });
+                    }
+                });
+            }
+        };
+    })
+
+
