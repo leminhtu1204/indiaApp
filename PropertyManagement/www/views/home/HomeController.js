ca045e6aee38876b0a18d98b3fa559bdc56f0f23
leminhtu1204 <tu.leminh@harveynash.vn>
2016-06-29 15:11:07 +0700

    Temporary Code

--- PropertyManagement/www/views/home/HomeController.js
+++ PropertyManagement/www/views/home/HomeController.js
@@ -1,25 +1,17 @@
-angular.module('Belowval.Home', [])
-
-    .controller('HomeController', function($scope, $state, $http) {
+angular.module('Belowval.Home', []).controller('HomeController', function ($scope, $state, $http) {
 
     ws_end_point = "http://underval.com/underval.com/engineermaster/api/api.php";
     $scope.homeData = [];
     $scope.init = function () {
-        var temKey = [];
         $http.post(ws_end_point, { "method": 3 }).success(function (data) {
             for (var item in data) {
-                temKey.push(item);
+                $scope.homeData.push(data[item]);
             }
 
-            for (var i = 0; i < temKey.length; i++) {
-                $scope.homeData.push(data[temKey[i]]);
-            }
-
-        }).error(function() {
+        }).error(function () {
 
         });
-
-    }
+    };
 
     if (window.localStorage.getItem('profile') != undefined) {
         $scope.init();
