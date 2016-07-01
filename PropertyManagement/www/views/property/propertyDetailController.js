efea25838555e1e5e951fb73a98a96b7ef91304a
leminhtu1204 <leminhtu12041988@gmail.com>
2016-07-02 00:36:18 +0700

    Tu

--- PropertyManagement/www/views/property/propertyDetailController.js
+++ PropertyManagement/www/views/property/propertyDetailController.js
@@ -1,24 +1,18 @@
-angular.module('Belowval.PropertyDetail', []).controller('PropertyDetailController', function ($scope, $state, $http, $stateParams) {
+angular.module('Belowval.PropertyDetail', []).controller('PropertyDetailController', function ($scope, $state, $http, $stateParams, $ionicSlideBoxDelegate) {
 
     ws_end_point = "http://underval.com/underval.com/engineermaster/api/api.php";
-    $scope.homeData = [];
-    $scope.init = function () {
-        $http.post(ws_end_point, { "method": 3 }).success(function (data) {
-            for (var item in data) {
-                $scope.homeData.push(data[item]);
-            }
 
+    $scope.init = function () {
+        $http.post(ws_end_point, { "method": 5, "propertyid": $stateParams.id }).success(function (data) {
+            $scope.property = data;
+            setTimeout(function () {
+                $ionicSlideBoxDelegate.slide(0);
+                $ionicSlideBoxDelegate.update();
+                $scope.$apply();
+            });
         }).error(function () {
 
         });
-    };
-
-    $scope.countPercent = function(a, b) {
-        return Math.round((a - b) / a * 100);
-    };
-
-    $scope.formatNumber = function(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     };
 
     if (window.localStorage.getItem('profile') != undefined) {
