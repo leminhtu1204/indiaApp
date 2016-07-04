cde11a29cc765eea9baff85c01772ca16ebbc338
leminhtu1204 <leminhtu12041988@gmail.com>
2016-07-04 23:57:54 +0700

    update search

--- PropertyManagement/www/js/app.js
+++ PropertyManagement/www/js/app.js
@@ -1,5 +1,5 @@
 angular.module('Belowval', ['ionic', 'Belowval.Bootstrap', 'Belowval.Login', 'Belowval.Home', 'Belowval.Register', 'Belowval.GroupProperty',
-    'Belowval.PropertyDetail', 'Belowval.Profile'])
+    'Belowval.PropertyDetail', 'Belowval.Profile', 'Belowval.Search'])
     .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('belowval', {
@@ -23,6 +23,15 @@ angular.module('Belowval', ['ionic', 'Belowval.Bootstrap', 'Belowval.Login', 'Be
                     'menuContent': {
                         controller: 'GroupPropertyController',
                         templateUrl: 'views/groupProperty/groupProperty.html'
+                    }
+                }
+            })
+            .state('belowval.search', {
+                url: '/search',
+                views: {
+                    'menuContent': {
+                        controller: 'SearchController',
+                        templateUrl: 'views/search/search.html'
                     }
                 }
             })
